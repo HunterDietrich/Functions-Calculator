05cdd29a04aa8328d149b5daba1a47480dc504c2
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-09 23:53:33 +0000

    finished addition

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 print("---Welcome to Calculator Program---")
 print()
-num1 = int("Provide a first number: ")
-num2 = int("Provide a second number: ")
-op = input("Provide an operator. Enter either +, -, /, * \n")
+num1 = int(input("Provide a first number: "))
+num2 = int(input("Provide a second number: "))
+op = input("Provide an operator. Choos one: \n +, -, /, * \n")
+
+
+def check_operator(op):
+    global num1, num2
+    if op == "+":
+        #addition
+        addition(num1, num2)
+    elif op == "-":
+        #subbtraction
+        pass
+    elif op == "/":
+        #division
+        pass
+    elif op == "*":
+        #multiplication
+        pass
+    else:
+        print("hmm... that's not an operation")
+
+def addition(n1, n2):
+            print(f"Sum is: {n1 + n2}")
+
+check_operator(op)
 
