f698abc363fea279f1f2dac3e665a6a6a2e83923
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-09 23:39:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("---Welcome to Calculator Program---")
+print()
+num1 = int("Provide a first number: ")
+num2 = int("Provide a second number: ")
+op = input("Provide an operator. Enter either +, -, /, * \n")
+
